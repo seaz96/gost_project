aecc1b5ff2b9d7f16afc36cc863b74fad5a96eda
seaz96 <seaz442@mail.ru>
2024-05-13 23:24:53 +0500

    Исправлен баг с загрузкой новых документов после первого запроса, когда нет возможности делать скролл вниз

--- client/src/pages/gosts-page/ui/index.tsx
+++ client/src/pages/gosts-page/ui/index.tsx
@@ -5,34 +5,49 @@ import styles from './GostsPage.module.scss'
 import { gostModel } from 'entities/gost';
 import { useGostsWithPagination } from 'entities/gost';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import {useEffect} from "react";
 
 const GostsPage = () => {
-  const {gosts, countFetched, count, setGostParams, fetchGostsData } = useGostsWithPagination('/docs/all-valid')
+    const {gosts, countFetched, count, setGostParams, fetchGostsData } = useGostsWithPagination('/docs/all-valid')
 
-  return (
-    <div className='container contentContainer'>
-        <InfiniteScroll
-          dataLength={countFetched} //This is important field to render the next data
-          next={fetchGostsData}
-          hasMore={count > countFetched}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: 'center' }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-        >
-          <section className={styles.filterSection}>
-            <Filter 
-              filterSubmit={(filterData: gostModel.GostFields & {name? :string}) => setGostParams(filterData)}
-            />
-          </section>
-          <section className={styles.gostSection}>
-            <GostsTable gosts={gosts || []} />
-          </section>
-        </InfiniteScroll>
-    </div>
-  )
+    useEffect(() => {
+        function checkScrollable() {
+            const { scrollHeight, clientHeight } =
+                document.documentElement;
+            const isContentScrollable = scrollHeight > clientHeight;
+            console.log(isContentScrollable);
+            if (!isContentScrollable && countFetched < count) {
+                fetchGostsData();
+            }
+        }
+        checkScrollable();
+        window.addEventListener("resize", checkScrollable);
+        return () => {
+            window.removeEventListener("resize", checkScrollable);
+        };
+    }, [gosts]);
+
+    return (
+        <div className='container contentContainer'>
+            <InfiniteScroll
+                dataLength={countFetched}
+                next={fetchGostsData}
+                hasMore={count > countFetched}
+                loader={<p style={{textAlign: 'center'}}>
+                    <b>Загрузка...</b>
+                </p>}
+            >
+                <section className={styles.filterSection}>
+                    <Filter
+                        filterSubmit={(filterData: gostModel.GostFields & {name? :string}) => setGostParams(filterData)}
+                    />
+                </section>
+                <section className={styles.gostSection}>
+                    <GostsTable gosts={gosts || []} />
+                </section>
+            </InfiniteScroll>
+        </div>
+    )
 }
 
 export default GostsPage;
