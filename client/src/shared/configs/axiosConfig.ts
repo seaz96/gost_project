2c45113b7f60969733addcdfa55358314667f6ea
seaz96 <seaz442@mail.ru>
2024-10-13 02:07:02 +0500

    fix axios url?

--- client/src/shared/configs/axiosConfig.ts
+++ client/src/shared/configs/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.API_URL
 
 const axiosInstance = axios.create({
-    baseURL: process.env.API_URL + '/'
+    baseURL: `${process.env.API_URL}/`
 });
 
 axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt_token')}`;
