4460a4a3f69f4b585b7673f9b1d0b0d2a2f72a02
kexogg <kexogg@gmail.com>
2025-01-07 02:45:20 +0500

    generic table component

---
+++ client/src/components/GenericTable/GenericTableActionBlock.tsx
@@ -0,0 +1,12 @@
+import type {ReactNode} from "react";
+import styles from "./GenericTable.module.scss";
+
+interface GenericTableActionBlockProps {
+	children?: ReactNode;
+}
+
+const GenericTableActionBlock = ({ children }: GenericTableActionBlockProps) => {
+	return <div className={styles.actions}>{children}</div>;
+};
+
+export default GenericTableActionBlock;
--- client/src/components/GostsTable/GostsTable.tsx
+++ client/src/components/GostsTable/GostsTable.tsx
@@ -1,103 +1,55 @@
 import {EditRounded, ImportContactsRounded} from "@mui/icons-material";
-import type {FC, ReactNode} from "react";
-import {Link} from "react-router-dom";
 import {useAppSelector} from "../../app/hooks.ts";
 import type {gostModel} from "../../entities/gost";
-import styles from "./GostTable.module.scss";
+import {GenericTable} from "../GenericTable/GenericTable.tsx";
+import GenericTableActionBlock from "../GenericTable/GenericTableActionBlock.tsx";
+import GenericTableButton from "../GenericTable/GenericTableButton.tsx";
 
 interface GostsTableProps {
 	gosts: gostModel.GostViewInfo[];
 	gostsParams: (gostModel.GostFields & { text?: string }) | null;
 }
 
-const GostsTable: React.FC<GostsTableProps> = (props) => {
-	const { gosts, gostsParams } = props;
-
-	return (
-		<table className={styles.table}>
-			<thead>
-				<tr className={styles.tableRow}>
-					<th>Код ОКС</th>
-					<th>Обозначение</th>
-					<th>Наименование</th>
-					{gostsParams && Object.values(gostsParams).some((param) => param) && <th>Соответствие запросу</th>}
-					<th>Действия</th>
-				</tr>
-			</thead>
-			{gosts.map((gost) => (
-				<GostRow key={gost.id} gostsParams={gostsParams} gost={gost} />
-			))}
-		</table>
-	);
-};
-
-interface GostRowProps {
-	gost: gostModel.GostViewInfo;
-	gostsParams: (gostModel.GostFields & { text?: string }) | null;
-}
-
-const GostRow: FC<GostRowProps> = ({ gost, gostsParams }) => {
-	//const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-	//const open = Boolean(anchorEl);
+const GostsTable = ({ gosts, gostsParams }: GostsTableProps) => {
 	const user = useAppSelector((s) => s.user.user);
+	const columns = [
+		{
+			header: "Код ОКС",
+			accessor: (g: gostModel.GostViewInfo) => g.codeOks,
+		},
+		{
+			header: "Обозначение",
+			accessor: (g: gostModel.GostViewInfo) => g.designation,
+		},
+		{
+			header: "Наименование",
+			accessor: (g: gostModel.GostViewInfo) => g.fullName,
+		},
+		gostsParams && Object.values(gostsParams).some((param) => param)
+			? {
+					header: "Соответствие запросу",
+					accessor: (g: gostModel.GostViewInfo) =>
+						gostsParams && Object.values(gostsParams).some((param) => param) ? g.relevanceMark : null,
+				}
+			: null,
+		{
+			header: "Действия",
+			accessor: (g: gostModel.GostViewInfo) => (
+				<GenericTableActionBlock>
+					<GenericTableButton to={`/gost-review/${g.id}`}>
+						<ImportContactsRounded />
+					</GenericTableButton>
+					{(user?.role === "Admin" || user?.role === "Heisenberg") && (
+						<GenericTableButton to={`/gost-edit/${g.id}`}>
+							<EditRounded />
+						</GenericTableButton>
+					)}
+				</GenericTableActionBlock>
+			),
+		},
+	].filter((column) => column !== null);
 
-	if (gost.designation)
-		return (
-			<tbody>
-				{/*<IconButton
-				id={gost.id.toString()}
-				className={styles.loop}
-				onClick={(event) => {
-					setAnchorEl(event.currentTarget);
-				}}
-			>
-				<img src={loop} alt="открыть сводку по сфере применения"/>
-			</IconButton>
-				<Popover
-				id={gost.id.toString()}
-			open={open}
-			onClose={() => setAnchorEl(null)}
-			anchorEl={anchorEl}
-			anchorOrigin={{
-				vertical: "bottom",
-				horizontal: "center",
-			}}
-			sx={{
-				".css-3bmhjh-MuiPaper-root-MuiPopover-paper": {},
-			}}
-			>
-			<div className={styles.applicationAreaContainer} style={{whiteSpace: "pre-line"}}>
-				{gost.applicationArea}
-			</div>
-			</Popover>
-*/}
-				<tr>
-					<td>{gost.codeOks}</td>
-					<td>{gost.designation}</td>
-					<td>{gost.fullName}</td>
-					{gostsParams && Object.values(gostsParams).some((param) => param) && <td>{gost.relevanceMark}</td>}
-					<td>
-						<div className={styles.actions}>
-							<TableButton to={`/gost-review/${gost.id}`}>
-								<ImportContactsRounded />
-							</TableButton>
-							{(user?.role === "Admin" || user?.role === "Heisenberg") && (
-								<TableButton to={`/gost-edit/${gost.id}`}>
-									<EditRounded />
-								</TableButton>
-							)}
-						</div>
-					</td>
-				</tr>
-			</tbody>
-		);
-	return <></>;
+	return <GenericTable columns={columns} data={gosts} rowKey={"id"} />;
 };
-
-const TableButton = ({ to, children }: { to: string; children: ReactNode }) => (
-	<Link to={to} className={styles.tableButton}>
-		{children}
-	</Link>
-);
 
 export default GostsTable;
